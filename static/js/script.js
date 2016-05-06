f3262dded6d23d1a35321debb95cfc768982c62f
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-05-06 08:49:22 -0700

    Better example code comments.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 // Your client side JavaScript code goes here.
 
+// Example code for creating a list on the server
 function createList(name, pos, cards) {
   return $.ajax('/api/lists', {
     type: 'POST',
@@ -11,33 +12,46 @@ function createList(name, pos, cards) {
   });
 }
 
+// Example code for getting all `list`s from server
 function loadLists() {
   return $.ajax('/api/lists');
+}
+
+// Example code for displaying lists in the browser
+function displayLists(lists) {
+  // Lists should be ordered based on their 'pos' field
+  lists.rows = _.sortBy(lists.rows, 'pos');
+  lists.rows.forEach(function(list) {
+    var curElem = $('<li>').text(list.name);
+    if (list.cards) {
+      var innerUl = $('<ul>');
+      list.cards.forEach(function(card) {
+        innerUl.append($('<li>').text(card));
+      });
+      curElem.append(innerUl);
+    }
+    $('#lists').append(curElem);
+  });
 }
 
 loadLists()
   .then(function(data) {
     console.log('Lists', data.rows);
-    // If no lists are found, create sample list
-    if (! data.rows.length) {
+    if (data.rows.length) {
+      // If some lists are found display them
+      displayLists(data);
+    } else {
+      // If no lists are found, create sample list
+      // and re-display.
       console.log('No lists found, creating one.');
       createList('Hello', 0, ['Card 1', 'Card 2'])
         .then(function(list) {
           console.log('Created list', list);
+          return loadLists();
+        })
+        .then(function(lists) {
+          displayLists(lists);
         })
     }
 
-    // Lists should be ordered based on their 'pos' field
-    data.rows = _.sortBy(data.rows, 'pos');
-    data.rows.forEach(function(list) {
-      var curElem = $('<li>').text(list.name);
-      if (list.cards) {
-        var innerUl = $('<ul>');
-        list.cards.forEach(function(card) {
-          innerUl.append($('<li>').text(card));
-        });
-        curElem.append(innerUl);
-      }
-      $('#lists').append(curElem);
-    });
   });
